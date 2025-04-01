f1d8846fe2a013cae7f73081ed8b29fb8f474bfa
Gustavo <peagudagustavo@gmail.com>
2025-04-01 09:10:23 -0300

    declaraão de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+//DECLARAÇÕES DE VARIAVEIS
+var nome = "fiap";
+console.log (nome);
+
+let idade = 18;
+console.log(idade)
+
+const sobrenome = "cidade"
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+console.log (exemplo1)
+
+let exemplo2=[];
+console.log(exemplo2)
