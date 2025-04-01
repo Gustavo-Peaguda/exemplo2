246223f5ca502b8461b600766d855fb79d78787e
Gustavo <peagudagustavo@gmail.com>
2025-04-01 09:52:50 -0300

    adicionando operadores logicos e aritimeticos

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,47 @@ let valor = null;
 console.log(valor)
 
 let exemplo1={};
-console.log (exemplo1)
-
 let exemplo2=[];
-console.log(exemplo2)
+//TIPOS DE VARIAVEIS
+let exemplo3 = 10;
+console.log (typeof exemplo3)
+let exemplo6 = ["mulher"]
+console.log (typeof exemplo6)
+
+//float => inteiro
+let numFloat = 123.456;
+console.log(parseInt(numFloat))
+
+//string => float
+let numString = "536.345"
+console.log (parseFloat(numString))
+
+let nunInt = 100;
+console.log(nunInt. toString())
+
+
+//metodos 
+//lenght - verifica o tamanho da string
+let frase = "o enzo é borepererre";
+console.log(frase.length)
+
+//indexOf -retorna um trecho de um texto
+let texto = "felipão"
+console.log(texto.indexOf("ão"))
+
+//slice - 
+let info = "lorenzo é muito chato"
+console.log(info.slice(1,2))
+
+//operadores aritimeticos
+
+const a=10;
+const b=20;
+console.log (a+b)
+console.log ((a+b)**99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999);
+
+//operadores logicos
+const c=20
+const d=30
+
+console.log(c<d)
