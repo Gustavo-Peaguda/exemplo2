5da189194082c3253a8760b9ae7344440452f046
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 08:46:17 -0300

    Adicionando operadores de comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,61 +1,71 @@
-//DECLARAÇÕES DE VARIAVEIS
-var nome = "fiap";
-console.log (nome);
+// //DECLARAÇÕES DE VARIAVEIS
+// var nome = "fiap";
+// console.log (nome);
 
-let idade = 18;
-console.log(idade)
+// let idade = 18;
+// console.log(idade)
 
-const sobrenome = "cidade"
-console.log(sobrenome)
+// const sobrenome = "cidade"
+// console.log(sobrenome)
 
-let aula;
-console.log(aula)
+// let aula;
+// console.log(aula)
 
-let valor = null;
-console.log(valor)
+// let valor = null;
+// console.log(valor)
 
-let exemplo1={};
-let exemplo2=[];
-//TIPOS DE VARIAVEIS
-let exemplo3 = 10;
-console.log (typeof exemplo3)
-let exemplo6 = ["mulher"]
-console.log (typeof exemplo6)
+// let exemplo1={};
+// let exemplo2=[];
+// //TIPOS DE VARIAVEIS
+// let exemplo3 = 10;
+// console.log (typeof exemplo3)
+// let exemplo6 = ["mulher"]
+// console.log (typeof exemplo6)
 
-//float => inteiro
-let numFloat = 123.456;
-console.log(parseInt(numFloat))
+// //float => inteiro
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat))
 
-//string => float
-let numString = "536.345"
-console.log (parseFloat(numString))
+// //string => float
+// let numString = "536.345"
+// console.log (parseFloat(numString))
 
-let nunInt = 100;
-console.log(nunInt. toString())
+// let nunInt = 100;
+// console.log(nunInt. toString())
 
 
-//metodos 
-//lenght - verifica o tamanho da string
-let frase = "o enzo é borepererre";
-console.log(frase.length)
+// //metodos 
+// //lenght - verifica o tamanho da string
+// let frase = "o enzo é borepererre";
+// console.log(frase.length)
 
-//indexOf -retorna um trecho de um texto
-let texto = "felipão"
-console.log(texto.indexOf("ão"))
+// //indexOf -retorna um trecho de um texto
+// let texto = "felipão"
+// console.log(texto.indexOf("ão"))
 
-//slice - 
-let info = "lorenzo é muito chato"
-console.log(info.slice(1,2))
+// //slice - 
+// let info = "lorenzo é muito chato"
+// console.log(info.slice(1,2))
 
-//operadores aritimeticos
+// //operadores aritimeticos
 
-const a=10;
-const b=20;
-console.log (a+b)
-console.log ((a+b)**99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999);
+// const a=10;
+// const b=20;
+// console.log (a+b)
+// console.log ((a+b)**10);
 
-//operadores logicos
-const c=20
-const d=30
+// //operadores logicos
+// const c=20
+// const d=30
 
-console.log(c<d)
+// console.log(c<d)
+// console.log(c < d && d<50)
+// console.log(c > d || d > c)
+// console.log(c == d || d <= c)
+
+//operadores de comparação
+const num5 = 25
+const num6 = 35
+console.log(num5 == num6) //compara
+console.log(num5 === num6) //compara e verifica o tipo da variavel
+console.log(num5 != num6) //diferente
